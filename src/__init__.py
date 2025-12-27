586eda8b232fdcc8e56498b6f1f842fc4819b416
Luis Ferrari <luisiferrari@gmail.com>
2025-12-27 11:29:09 -0300

    Adicionado a conexção para DB no postgres e as funções async

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,23 @@
 from sys import version
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Server is starting...")
+    await init_db()
+    yield
+    print(f"Server has been stoped.")
 
 version = "v1"
+
 app = FastAPI(
     title="Book Management API",
     description="An API to manage a collection of books.",
     version=version,
-    
+    lifespan=life_span
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["Books"])
